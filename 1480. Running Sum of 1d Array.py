da5bfd1ef6bba220ff91681fd091aa4f48a3a42b
zchenpy <89493901+zchenpy@users.noreply.github.com>
2022-05-12 00:37:09 -0500

    Update 1480. Running Sum of 1d Array.py

--- 1480. Running Sum of 1d Array.py
+++ 1480. Running Sum of 1d Array.py
@@ -1,1 +1,13 @@
-
+# DP
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        dp = [0]*n    
+        
+        dp[0] = nums[0] 
+        for i in range(1,n):
+            dp[i] = dp[i-1]+nums[i]
+            
+        return dp
+#Time Complexity: O(n)
+#Space Complexity: O(n)
