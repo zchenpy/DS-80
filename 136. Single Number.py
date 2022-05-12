8a7fd4f6b0f2d35b41d239e0d2ea9d69328e141b
zchenpy <89493901+zchenpy@users.noreply.github.com>
2022-05-11 20:10:49 -0500

    Create 136. Single Number.py

---
+++ 136. Single Number.py
@@ -0,0 +1,13 @@
+# 1 list and set
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return 2*sum(set(nums))-sum(nums)
+
+#Time Complexity: 
+#Space Complexity:
+
+-------------------------------------------------------------
+# 2 
+      
+      
+      
