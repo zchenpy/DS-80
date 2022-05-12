6db2e547bfe95d1ada46b11f72e1b11b1d5214da
zchenpy <89493901+zchenpy@users.noreply.github.com>
2022-05-12 00:26:51 -0500

    Update 0001. Two Sum.py

--- 0001. Two Sum.py
+++ 0001. Two Sum.py
@@ -1,1 +1,12 @@
+#1 Hashtable
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashtable={}
+        for i in range(len(nums)):
+            if target-nums[i] in hashtable:
+                return[hashtable[target-nums[i]],i]
+            else:
+                hashtable[nums[i]] = i
+#Time complexity: O(n)
+#Space complexity:O(n)
