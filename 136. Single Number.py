780e1a5cad9883fddc20c74f40088688ac9aef0c
zchenpy <89493901+zchenpy@users.noreply.github.com>
2022-05-11 22:34:15 -0500

    Update 136. Single Number.py

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -1,13 +1,25 @@
-# 1 list and set
+# 1 hashtable
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hashtable ={}
+        for e in nums:
+            hashtable[e] = hashtable.get(e,0) +1 
+           
+        for key in hashtable: 
+            if hashtable[key]==1:
+                return key
+#Time Complexity:O(n)
+#Space Complexity:O(n)
+
+-------------------------------------------------------------
+# 2 list and set
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         return 2*sum(set(nums))-sum(nums)
 
-#Time Complexity: 
-#Space Complexity:
-
--------------------------------------------------------------
-# 2 
+#Time Complexity:2*O(n)
+#Space Complexity:O(n)
       
       
       
