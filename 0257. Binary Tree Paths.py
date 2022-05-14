d33a6089c8e173ea9dac336323a5cb9605a82a84
zchenpy <89493901+zchenpy@users.noreply.github.com>
2022-05-14 17:16:12 -0500

    Update and rename 257. Binary Tree Paths.py to 0257. Binary Tree Paths.py

--- 0257. Binary Tree Paths.py
+++ 0257. Binary Tree Paths.py
@@ -1,4 +1,4 @@
-# Recursion
+#1 DFS-Recursion
 
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
@@ -17,5 +17,29 @@ class Solution:
         if root.right:                                         #if root.right, update current_path
             self.dfs(root.right,output,current_path+'->')
 
-#Time complexity: O(n)
-#Space complexity:O(n)
+#Time complexity: O(n2)
+#Space complexity:O(n2)
+#Worst case: linked node. Path will be recorded for each node. Tree structure: nlog(n)
+
+
+#2 DFS-Stack
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        if not root:
+            return []
+        
+        output = []
+        stack = [(root,''),]
+        while stack:
+            check,path = stack.pop()
+            if not check.left and not check.right:
+                output.append(path + str(check.val))
+            
+            if check.right:                                            #push child into stack，record current path
+                stack.append((check.right, path+str(check.val)+'->'))  #tuple，(())
+            if check.left:
+                stack.append((check.left, path+str(check.val)+'->'))
+        return output
+    
+#Time complexity: O(n2)
+#Space complexity:O(n2)
