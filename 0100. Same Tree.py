b03d771a574b518a29f4000211f1de6117c70cb8
zchenpy <89493901+zchenpy@users.noreply.github.com>
2022-05-16 23:32:27 -0500

    Update 0100. Same Tree.py

--- 0100. Same Tree.py
+++ 0100. Same Tree.py
@@ -1,3 +1,5 @@
+https://leetcode.com/problems/same-tree/
+    
 #1. Recursion:
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
@@ -14,3 +16,17 @@ class Solution:
 #Time complexity:  O(min(n,m))
 #Space complexity: O(min(n,m)) for extra stack space in recursion
 
+from collections import deque
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        queue = deque([(p,q),])       
+        while queue:
+            (p,q)=queue.popleft()
+            if not p and not q:
+                continue
+            elif p and q and p.val==q.val:
+                queue.append((p.left,q.left))
+                queue.append((p.right,q.right))
+            else: return False
+        return True    
+                
