87fde062de8eb3bca5d4b7006b50c332d297689b
zchenpy <89493901+zchenpy@users.noreply.github.com>
2022-05-20 07:15:52 -0500

    Update 0268. Missing Number.py

--- 0268. Missing Number.py
+++ 0268. Missing Number.py
@@ -1,3 +1,6 @@
+https://leetcode.com/problems/missing-number/
+
+#1. Hashtable
 class Solution(object):
     def missingNumber(self, nums):
         """
@@ -10,3 +13,16 @@ class Solution(object):
               
 #Time complexity: O(n)
 #Space complexity:O(n)
+
+
+
+#2. Math
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        sum_ap = n*(1+n)//2                    #Sum of Arithmetic Progression
+        return sum_ap - sum(nums)
+    
+                  
+#Time complexity: O(n)
+#Space complexity:O(1)
